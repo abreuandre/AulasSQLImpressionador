cabc55427af72c597b566c8258eb4e8512446823
Andre Abreu <andreluismachadoabreu@gmail.com>
2025-01-03 15:51:41 -0300

    Adicionando Arquivo Minhas consultas SQL

---
+++ Minhas consultas SQL.sql
@@ -0,0 +1,9 @@
+SELECT * FROM DimCustomer  /* Seleciona todas as colunas da tabela DimCustomer */
+
+SELECT * FROM DimStore /* Seleciona todas as colunas da tabela DimStore */
+
+SELECT StoreKey, StoreName, StorePhone FROM DimStore /* Seleciona as colunas StoreKey, StoreName e StorePhone da tabela DimStore */
+
+SELECT * FROM DimProduct
+
+SELECT ProductName, BrandName FROM DimProduct
