b62d31f4e759b3bcb7859cdc56c55f6075738714
Andre Abreu <andreluismachadoabreu@gmail.com>
2025-01-03 16:09:03 -0300

    Atualização do arquivo Minhas consultas SQL.sql

--- Minhas consultas SQL.sql
+++ Minhas consultas SQL.sql
@@ -7,3 +7,18 @@ SELECT StoreKey, StoreName, StorePhone FROM DimStore /* Seleciona as colunas Sto
 SELECT * FROM DimProduct
 
 SELECT ProductName, BrandName FROM DimProduct
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+-- Comandos SELECT TOP(N) e TOP(N) PERCENT: retorna as N primeiras linhas
+
+-- 1. Crie um código que retorna as 10 primeiras linhas da tabela de Produtos.
+
+SELECT TOP(10) * FROM DimProduct
+
+-- 2. Retorna as 10% primeiras linhas da tabella de Clientes.
+
+SELECT TOP(10) PERCENT * FROM DimCustomer
+
+SELECT TOP(1000) * FROM FactSales
